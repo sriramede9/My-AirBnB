11273f122ff4103e6d8e24a2c8032369ac607bd5
sriramede9 <only4programming03@gmail.com>
2020-07-16 14:45:10 +0100

    fixed routing issue

--- src/app/places/places-module.ts
+++ src/app/places/places-module.ts
@@ -82,12 +82,19 @@ const routes: Routes = [
         children: [
           {
             path: "",
-            loadChildren: "./discover/discover.module#DiscoverPageModule",
+            // loadChildren: "./discover/discover.module#DiscoverPageModule",
+            loadChildren: () =>
+              import("./discover/discover.module").then(
+                (m) => m.DiscoverPageModule
+              ),
           },
           {
             path: ":placeId",
-            loadChildren:
-              "./discover/place-detail/place-detail.module#PlaceDetailPageModule",
+            // loadChildren:"./discover/place-detail/place-detail.module#PlaceDetailPageModule",
+            loadChildren: () =>
+              import("./discover/place-detail/place-detail.module").then(
+                (m) => m.PlaceDetailPageModule
+              ),
           },
         ],
       },
@@ -96,22 +103,33 @@ const routes: Routes = [
         children: [
           {
             path: "",
-            loadChildren: "./offers/offers.module#OffersPageModule",
+            // loadChildren: "./offers/offers.module#OffersPageModule",
+            loadChildren: () =>
+              import("./offers/offers.module").then((m) => m.OffersPageModule),
           },
           {
             path: "new",
-            loadChildren:
-              "./offers/new-offer/new-offer.module#NewOfferPageModule",
+            // loadChildren: "./offers/new-offer/new-offer.module#NewOfferPageModule",
+            loadChildren: () =>
+              import("./offers/new-offer/new-offer.module").then(
+                (m) => m.NewOfferPageModule
+              ),
           },
           {
             path: "edit/:placeId",
-            loadChildren:
-              "./offers/edit-offer/edit-offer.module#EditOfferPageModule",
+            // loadChildren:"./offers/edit-offer/edit-offer.module#EditOfferPageModule",
+            loadChildren: () =>
+              import("./offers/edit-offer/edit-offer.module").then(
+                (m) => m.EditOfferPageModule
+              ),
           },
           {
             path: ":placeId",
-            loadChildren:
-              "./offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule",
+            // loadChildren:"./offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule",
+            loadChildren: () =>
+              import("./offers/offer-bookings/offer-bookings.module").then(
+                (m) => m.OfferBookingsPageModule
+              ),
           },
         ],
       },
